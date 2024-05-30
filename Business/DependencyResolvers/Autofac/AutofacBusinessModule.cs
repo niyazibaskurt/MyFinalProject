bf3b5929d136236a078f0db11101ccc1051b398b
niyazi.baskurt@spk.gov.tr <niyazi.baskurt@spk.gov.tr>
2024-05-30 10:54:10 +0300

    Autofac ve Autofac.Extras paketleri yüklendi varsayılan DI olarak kulllanılması için

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+
+{    //Autofac kullanarak bağımlılık enjeksiyonu için bir yapılandırma modülü 
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Birisi senden IProductService isterse ona bir ProductManager instance'ı ver. 
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+            //uygulama boyunca IProductDal talep edildiğinde hep aynı EfProductDal örneği kullanılacak
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -8,9 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 
+//IoC varsayýlan baðýmlýlýk enjeksiyonu (Dependency Injection, DI) yapýsýný Autofac ile deðiþtir
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()); 
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
