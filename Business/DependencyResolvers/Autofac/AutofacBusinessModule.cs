dd848f1a65eaedb898fe4f33c5a3fb870adb185f
niyazi.baskurt@spk.gov.tr <niyazi.baskurt@spk.gov.tr>
2024-05-31 12:29:32 +0300

    31.05.2024

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -11,15 +14,23 @@ using System.Threading.Tasks;
 
 namespace Business.DependencyResolvers.Autofac
 
-{    //Autofac kullanarak bağımlılık enjeksiyonu için bir yapılandırma modülü 
+{    //Autofac kullanarak bağımlılık enjeksiyonu (Dependency Injection) için bir yapılandırma modülü 
     public class AutofacBusinessModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            //Birisi senden IProductService isterse ona bir ProductManager instance'ı ver. 
+            //Birisi IProductService isterse ona bir ProductManager instance'ı ver. 
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
             //uygulama boyunca IProductDal talep edildiğinde hep aynı EfProductDal örneği kullanılacak
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
