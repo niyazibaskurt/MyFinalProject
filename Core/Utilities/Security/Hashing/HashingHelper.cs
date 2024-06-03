d2663d74d27fbcef4d3c3adaef48cfa3705f5ab9
niyazi.baskurt@spk.gov.tr <niyazi.baskurt@spk.gov.tr>
2024-06-03 15:52:52 +0300

    3 Haziran Commit 2

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //İlk başta girilen bir parolanın hash ve salt değerinin oluşturulması
+        public static void CreatePasswordHash
+            (string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+
+        //Sonradan Girilen parolanın hash ve salt değerinin veri kaynağındaki bilgilerle karşılaştırılması
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+
+    }
+}
